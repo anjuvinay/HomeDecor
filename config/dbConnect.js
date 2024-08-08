cdba5508b20f33631cf5655de8ab8d508bbec135
anjuvinay <aanjups88@gmail.com>
2024-08-08 19:16:01 +0530

    mongodb instance created

--- config/dbConnect.js
+++ config/dbConnect.js
@@ -14,7 +14,7 @@ const { default:mongoose }=require("mongoose")
 
 const dbConnect= ()=>{
     try{
-        const conn=mongoose.connect("mongodb://localhost:27017/decorDB")
+        const conn=mongoose.connect("mongodb+srv://mongodb:mongodb@cluster0.o1red.mongodb.net/")
         console.log("Database connected successfully")
     }
     catch(error){
