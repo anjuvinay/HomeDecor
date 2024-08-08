6ff7ec70050d471fa90c31a926083ca8f023f891
anjuvinay <aanjups88@gmail.com>
2024-08-08 20:03:41 +0530

    db modified

--- config/dbConnect.js
+++ config/dbConnect.js
@@ -1,27 +1,36 @@
 
-// const mongoose=require('mongoose')
-
-// mongoose.connect("mongodb://localhost:27017/decorDB")
-// .then(()=>{
-//     console.log("Home_decor connected to database")
-// })
-// .catch(()=>{
-//     console.log("failed to connect")
-// })
 
 
-const { default:mongoose }=require("mongoose")
+// const { default:mongoose }=require("mongoose")
 
-const dbConnect= ()=>{
-    try{
-        const conn=mongoose.connect("mongodb+srv://mongodb:mongodb@cluster0.o1red.mongodb.net/")
-        console.log("Database connected successfully")
-    }
-    catch(error){
-        console.log("failed to connect")
-    }
+// const dbConnect= ()=>{
+//     try{
+//         const conn=mongoose.connect("mongodb+srv://mongodb:mongodb@cluster0.o1red.mongodb.net/")
+//         console.log("Database connected successfully")
+//     }
+//     catch(error){
+//         console.log("failed to connect")
+//     }
     
-}
+// }
 
 
-module.exports=dbConnect
+// module.exports=dbConnect
+
+
+
+const mongoose = require("mongoose");
+
+const dbConnect = async () => {
+    try {
+        await mongoose.connect("mongodb+srv://mongodb:mongodb@cluster0.o1red.mongodb.net/your-database-name", {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        });
+        console.log("Database connected successfully");
+    } catch (error) {
+        console.error("Failed to connect to the database:", error);
+    }
+};
+
+module.exports = dbConnect;
