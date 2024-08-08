f15f52c1d1b9bb7aef2074ae6384de39b4a054ef
anjuvinay <aanjups88@gmail.com>
2024-08-08 22:34:06 +0530

    db config changed

--- config/dbConnect.js
+++ config/dbConnect.js
@@ -1,36 +1,23 @@
 
 
 
-// const { default:mongoose }=require("mongoose")
+const { default:mongoose }=require("mongoose")
 
-// const dbConnect= ()=>{
-//     try{
-//         const conn=mongoose.connect("mongodb+srv://mongodb:mongodb@cluster0.o1red.mongodb.net/")
-//         console.log("Database connected successfully")
-//     }
-//     catch(error){
-//         console.log("failed to connect")
-//     }
-    
-// }
-
-
-// module.exports=dbConnect
-
-
-
-const mongoose = require("mongoose");
-
-const dbConnect = async () => {
-    try {
-        await mongoose.connect("mongodb+srv://mongodb:mongodb@cluster0.o1red.mongodb.net/your-database-name", {
-            useNewUrlParser: true,
-            useUnifiedTopology: true
-        });
-        console.log("Database connected successfully");
-    } catch (error) {
-        console.error("Failed to connect to the database:", error);
+const dbConnect= ()=>{
+    try{
+        const conn=mongoose.connect("mongodb://localhost:27017/decorDB")
+        //const conn=mongoose.connect("mongodb+srv://mongodb:mongodb@cluster0.o1red.mongodb.net/decorDB")
+        console.log("Database connected successfully")
     }
-};
+    catch(error){
+        console.log("failed to connect")
+    }
+    
+}
 
-module.exports = dbConnect;
+
+module.exports=dbConnect
+
+
+
+
